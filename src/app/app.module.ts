0d7e92c7e917b21e1c4632f2e972d3242c2a8ec5
James Scott-Nicholson <james.scott-nicholson@objective-partner.com>
2021-03-04 11:05:54 +0100

    first

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputComponent } from './game-container/input/input.component';
+import { GameContainerComponent } from './game-container/game-container.component';
+import { SuccessComponent } from './game-container/success/success.component';
+import { WordComponent } from './game-container/word/word.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WordComponent,
+    InputComponent,
+    GameContainerComponent,
+    SuccessComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
